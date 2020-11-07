90c9929c19d4f8db7280b3f994ffc6bbdf96ba84
Maday Moya <maday.moya001@mymdc.net>
2020-11-07 18:14:32 -0500

    updated

--- SolidShape.java
+++ SolidShape.java
@@ -1,4 +1,26 @@
 
 
-public class SolidShape {
+public class SolidShape
+{
+    String color; //specify the color of the solid shape
+
+    SolidShape(){} // no-arg constructor that creates a default solid shape.
+
+    SolidShape(String col){  // constructor that creates a solid shape with the specified color.
+
+        color=col;
+    }
+
+    //The setter and getter methods for the data field.
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    abstract double getArea();
+    abstract double getVolume();
+
 }
